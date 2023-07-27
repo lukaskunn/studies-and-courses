c1fff4c6ef59c4d08eba79e8d3fb12e897bfa8ef
lukaskunn <lucassioliveira098@gmail.com>
2023-07-26 22:10:14 -0300

    feat: started work with Dynamic names in Django

--- courses/Django-persistencia-de-dados-e-admin/galeria/views.py
+++ courses/Django-persistencia-de-dados-e-admin/galeria/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados  ={
+        1: {
+            "nome": "Nebulosa de Carina",
+            "legenda": "Webbtelescope.org / NASA / James Webb"
+        },
+        2: {
+            "nome": "Galaxia NGC 1070",
+            "legenda": "nasa.org / NASA / Hubble"
+        }
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
